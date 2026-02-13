a868f1f8e7a791bf2dcb91cac9ee7e69993459de
Andrei Volt <andrei@avolt.net>
2026-02-13 23:50:49 +0100

    add flake.nix and stable extension key for nix packaging

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in {
+      packages = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "sci-hub-now";
+            version = "0-unstable";
+            src = self;
+            dontBuild = true;
+            installPhase = ''
+              mkdir -p $out/share/chromium-extension
+              cp -r manifest.json service_worker.js options.html options.js \
+                    browser-polyfill.js icons data helper_js \
+                    $out/share/chromium-extension/
+            '';
+          };
+        });
+    };
+}
