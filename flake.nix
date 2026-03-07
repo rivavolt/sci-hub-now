44b3faa35241512086b04cacb76ad70cdde030ee
Andrei Volt <andrei@avolt.net>
2026-03-07 17:22:45 +0100

    self-contained CRX signing via PEM key in flake

--- flake.nix
+++ flake.nix
@@ -6,9 +6,10 @@
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in {
       packages = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          default = pkgs.stdenv.mkDerivation {
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          extension = pkgs.stdenv.mkDerivation {
             pname = "sci-hub-now";
             version = "0-unstable";
             src = self;
@@ -20,6 +21,32 @@
                     $out/share/chromium-extension/
             '';
           };
+
+          manifest = builtins.fromJSON (builtins.readFile "${extension}/share/chromium-extension/manifest.json");
+
+          extId = builtins.readFile (pkgs.runCommand "sci-hub-now-ext-id" {
+            nativeBuildInputs = [ pkgs.python3 pkgs.openssl ];
+          } ''
+            python3 ${./nix/crx-id.py} ${./keys/signing.pem} > $out
+          '');
+
+          crx = pkgs.runCommand "sci-hub-now-crx" {
+            nativeBuildInputs = [ pkgs.python3 pkgs.openssl ];
+          } ''
+            mkdir -p $out
+            python3 ${./nix/pack-crx3.py} ${extension}/share/chromium-extension ${./keys/signing.pem} $out/extension.crx
+          '';
+
+        in {
+          inherit extension;
+          default = pkgs.linkFarm "sci-hub-now" [
+            { name = "share/chromium/extensions/${extId}.json";
+              path = pkgs.writeText "${extId}.json" (builtins.toJSON {
+                external_crx = "${crx}/extension.crx";
+                external_version = manifest.version;
+              });
+            }
+          ];
         });
     };
 }
