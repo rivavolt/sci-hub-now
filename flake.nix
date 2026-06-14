31d4727fe7d546e361bf8cb19316a89a6ca57a7f
Andrei Volt <andrei@avolt.net>
2026-06-14 07:46:39 +0200

    wip-snapshot riva 2026-06-14T05:46:39Z

--- flake.nix
+++ flake.nix
@@ -1,13 +1,7 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-crx = {
-      url = "github:andreivolt/nix-crx";
-      flake = true;
-    };
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, nix-crx }:
+  outputs = { self, nixpkgs }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in {
@@ -28,14 +22,31 @@
             '';
           };
 
-          crxPkg = nix-crx.lib.mkCrxPackage {
-            inherit pkgs extension;
-            key = ./keys/signing.pem;
-          };
+          manifest = builtins.fromJSON (builtins.readFile "${extension}/share/chromium-extension/manifest.json");
+
+          extId = builtins.readFile (pkgs.runCommand "sci-hub-now-ext-id" {
+            nativeBuildInputs = [ pkgs.python3 pkgs.openssl ];
+          } ''
+            python3 ${./nix/crx-id.py} ${./keys/signing.pem} > $out
+          '');
+
+          crx = pkgs.runCommand "sci-hub-now-crx" {
+            nativeBuildInputs = [ pkgs.python3 pkgs.openssl ];
+          } ''
+            mkdir -p $out
+            python3 ${./nix/pack-crx3.py} ${extension}/share/chromium-extension ${./keys/signing.pem} $out/extension.crx
+          '';
 
         in {
           inherit extension;
-          default = crxPkg.package;
+          default = pkgs.linkFarm "sci-hub-now" [
+            { name = "share/chromium/extensions/${extId}.json";
+              path = pkgs.writeText "${extId}.json" (builtins.toJSON {
+                external_crx = "${crx}/extension.crx";
+                external_version = manifest.version;
+              });
+            }
+          ];
         });
     };
 }
